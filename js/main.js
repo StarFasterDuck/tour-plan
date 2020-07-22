a64f1f12f671fd0212e4fa80a389e8c68aea6a8a
StarFasterDuck <alex1991163@mail.ru>
2020-07-22 17:48:38 +0300

    create: add main.js Keyboard Control

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 var mySwiper = new Swiper('.swiper-container', {
   // Optional parameters
   loop: true,
-
+  
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
   // Navigation arrows
   navigation: {
     nextEl: '.slider-button--next',
